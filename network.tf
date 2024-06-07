a14508d192deb8edb2f6d15ab0c0f68a02fde799
manuellatorreaodemenezes <manuella.torreu00e3o-ext@ab-inbev.com>
2024-06-07 15:34:21 -0300

    created outputs and route table

--- network.tf
+++ network.tf
@@ -81,4 +81,37 @@ resource "aws_nat_gateway" "my_nat_gateway" {
   }
 }
 
+resource "aws_route_table" "public_route_table" {
+  vpc_id  = aws_vpc.my_vpc.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.my_internet_gateway.id 
+  }
+  tags = {
+    Name = "${var.prefix}-public-route-table"
+  }
+}
+
+resource "aws_route_table" "private_route_table" {
+  vpc_id  = aws_vpc.my_vpc.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.my_nat_gateway.id
+  }  
+  tags = {
+    Name = "${var.prefix}-private-route-table"
+  }
+}
+
+#associar subnet pública á tabela de rota publica
+resource "aws_route_table_association" "public_subnet_1_association" {
+  subnet_id = aws_subnet.public-subnet1.id
+  route_table_id = aws_route_table.public_route_table.id
+}
+
+resource "aws_route_table_association" "private_subnet_1_association" {
+  subnet_id = aws_subnet.private-subnet2.id
+  route_table_id = aws_route_table.private_route_table.id
+}
+
 
