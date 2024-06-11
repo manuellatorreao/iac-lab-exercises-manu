485bc1e2e02f35d87433ca81da3e2c5bfd451853
manuellatorreaodemenezes <manuella.torreu00e3o-ext@ab-inbev.com>
2024-06-10 23:42:49 -0300

    refactor and includes new resources

--- variables.tf
+++ variables.tf
@@ -13,36 +13,6 @@ variable "vpc_cidr" {
   description = "The CIDR block for the VPC"
 }
 
-variable "subnet1_cidr" {
-  type        = string
-  description = "The CIDR block for subnet 1"
-}
-
-variable "subnet2_cidr" {
-  type        = string
-  description = "The CIDR block for subnet 2"
-}
-
-variable "subnet3_cidr" {
-  type        = string
-  description = "The CIDR block for subnet 3"
-}
-
-variable "subnet4_cidr" {
-  type        = string
-  description = "The CIDR block for subnet 4"
-}
-
-variable "subnet5_cidr" {
-  type        = string
-  description = "The CIDR block for subnet 5"
-}
-
-variable "subnet6_cidr" {
-  type        = string
-  description = "The CIDR block for subnet 6"
-}
-
 variable "number_public_subnets" {
   default = 2
 }
